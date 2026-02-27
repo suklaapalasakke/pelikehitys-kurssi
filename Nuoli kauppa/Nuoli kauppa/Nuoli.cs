4ede607a0e37eb352ee3f676e2f001edc1a01c11
suklaapalasakke <zakariya.guettaf@gmail.com>
2026-02-27 11:45:45 +0200

    nuoli tehtävä loppuun

---
+++ Nuoli kauppa/Nuoli kauppa/Nuoli.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nuoli_kauppa
+{
+    public enum KarkiTyyppi
+    {
+        Puu,
+        Teras,
+        Timantti
+    }
+
+    public enum PeraTyyppi
+    {
+        Lehti,
+        KananSulka,
+        KotkanSulka
+    }
+
+    public class Nuoli
+    {
+        public KarkiTyyppi karki;
+        public PeraTyyppi pera;
+        public double pituus;
+
+        public Nuoli(KarkiTyyppi k, PeraTyyppi p, double pit)
+        {
+            karki = k;
+            pera = p;
+            pituus = pit;
+        }
+
+        public double PalautaHinta()
+        {
+            double hinta = 0;
+
+            // Kärjen hinta
+            if (karki == KarkiTyyppi.Puu)
+            {
+                hinta = hinta + 3;
+            }
+            else if (karki == KarkiTyyppi.Teras)
+            {
+                hinta = hinta + 5;
+            }
+            else if (karki == KarkiTyyppi.Timantti)
+            {
+                hinta = hinta + 50;
+            }
+
+            // Perän hinta
+            if (pera == PeraTyyppi.Lehti)
+            {
+                hinta = hinta + 0;
+            }
+            else if (pera == PeraTyyppi.KananSulka)
+            {
+                hinta = hinta + 1;
+            }
+            else if (pera == PeraTyyppi.KotkanSulka)
+            {
+                hinta = hinta + 5;
+            }
+
+            // Varren hinta
+            hinta = hinta + (pituus * 0.05);
+
+            return hinta;
+        }
+    }
+}
---
+++ Nuoli kauppa/Nuoli kauppa/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Nuoli_kauppa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minkälainen kärki (Puu, Teräs, Timantti)?");
+            string karkiInput = Console.ReadLine();
+            KarkiTyyppi karki = Enum.Parse<KarkiTyyppi>(karkiInput);
+
+            Console.WriteLine("Minkälaiset sulat (Lehti, KananSulka, KotkanSulka)?");
+            string peraInput = Console.ReadLine();
+            PeraTyyppi pera = Enum.Parse<PeraTyyppi>(peraInput);
+
+            Console.WriteLine("Nuolen pituus sentteinä (60-100)?");
+            double pituus = Convert.ToDouble(Console.ReadLine());
+
+            Nuoli nuoli = new Nuoli(karki, pera, pituus);
+
+            double hinta = nuoli.PalautaHinta();
+
+            Console.WriteLine("Tämän nuolen hinta on " + hinta + " kultarahaa.");
+
+            Console.ReadLine();
+        }
+    }
+}
